42118b4976faa77a68204e971bf08d93aa877dac
doGior <43815981+doGior@users.noreply.github.com>
2020-05-14 17:28:01 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # Use your own values from my.telegram.org
-api_id = 1338682
-api_hash = '0df556d77e4c48bf734fc4448fd4c899'
+api_id = 12345
+api_hash = '0123456789abcdef0123456789abcdef'
 
 #Chat from where tou want to download the files
-chat = "https://t.me/joinchat/AAAAAE0pUzvX057hFf8vdA"
+chat = "chat link or username or id ord contact"
 
 #Folder in which files will be stored
 download_folder = "Downloads/"
