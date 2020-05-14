0658e3cde1bf5e026383b1a53814c27aab0b8441
doGior <43815981+doGior@users.noreply.github.com>
2020-05-14 17:24:12 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # Use your own values from my.telegram.org
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
+api_id = 1338682
+api_hash = '0df556d77e4c48bf734fc4448fd4c899'
 
 #Chat from where tou want to download the files
-chat = "chat link or username or id ord contact"
+chat = "https://t.me/joinchat/AAAAAE0pUzvX057hFf8vdA"
 
 #Folder in which files will be stored
-download_folder = "download/"
+download_folder = "Downloads/"
