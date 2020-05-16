c91a113333ffafd0435d01ef2f83bb4ba668f8ff
Riccardo Giordano <43815981+doGior@users.noreply.github.com>
2020-05-16 20:48:13 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,8 +2,19 @@
 api_id = 12345
 api_hash = '0123456789abcdef0123456789abcdef'
 
-#Chat from where tou want to download the files
-chat = "chat link or username or id ord contact"
+# Chat from where you want to download the files
+chat = "chat link or username or id or phone's number"
 
-#Folder in which files will be stored
+# Folder in which files will be stored
 download_folder = "Downloads/"
+
+
+
+# DANGER
+# Commands are linked to the functions by position,
+# so if you want to change the command for download all the
+# media from a chat you must be sure that the new command is
+# in the exact previous command's position in this list
+cmds = ["/downloadAll", "/downloadToday",
+        "/downloadImages", "/downloadVideo","/downloadMusic",
+        "/downloadWhenArrive", "/stopDownloadWhenArrive"]
