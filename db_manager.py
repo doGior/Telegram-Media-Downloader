3109891999cb9c1a8bf656f93cb32849ec3bf8f3
Riccardo Giordano <43815981+doGior@users.noreply.github.com>
2020-05-16 19:22:11 +0200

    Add files via upload

--- db_manager.py
+++ db_manager.py
@@ -2,27 +2,64 @@ import sqlite3
 
 
 def create_connection(db_file):
-    """ Create a database connection to a SQLite database """
+    """ Create a database connection to a SQLite database
+    db_file: database file name
+    return: Connection object """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print("Database Created")
+        print("Connection Established")
         return conn
     except sqlite3.Error as e:
         print(e)
+    
+    return conn
 
 def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
+    """ Create a table from the create_table_sql statement
+    param conn: Connection object
+    param create_table_sql: a CREATE TABLE statement
+    return:
     """
     try:
         c = conn.cursor()
         c.execute(create_table_sql)
-        c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
         conn.commit()
-        conn.close()
-        print("Database written - SQLite version ",sqlite3.version)
+        #conn.close()
+        print("Database written - SQLite version",sqlite3.version)
     except sqlite3.Error as e:
         print(e)
+
+
+def insert_message(conn, values):
+    """
+    Insert a new row into the id_to_file table
+    conn: Connection object
+    values: tuple of string
+    return: table id
+    """
+    sql = ''' INSERT INTO id_to_file(Message_Id, File_Path)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, values)
+    return cur.lastrowid
+
+
+def delete_message(conn, id):
+    """
+    Delete a row into from the id_to_file
+    table by message id
+    conn: Connection object
+    id: message id
+    return: table id
+    """
+    sql1 = "SELECT File_Path FROM id_to_file WHERE Message_Id=?"
+    sql2 = "DELETE FROM id_to_file WHERE Message_Id=?"
+    cur = conn.cursor()
+    cur.execute(sql1, (id,))
+    file_path = cur.fetchone()[0]
+    cur.execute(sql2, (id,))
+    conn.commit()
+    print("Row Deleted")
+    return file_path
+
--- download.py
+++ download.py
@@ -2,19 +2,56 @@ import asyncio
 from os import listdir, stat, mkdir, path
 from re import sub
 from settings import *
+import db_manager as dbm
+
+async def _download(client, message, db_conn):
+    message_id = message.id
+    message = message.document
+    file_name = message.attributes[-1].file_name
+
+    #Delete tags in file name if there are
+    file_name = sub('^\.*@[A-Za-z0-9]+ ', '', file_name) 
+
+    #Path to file
+    file = SortingFolder(message.mime_type, download_folder)
+    file += file_name
+
+    #Skip file already downloaded
+    if(file_name in listdir(download_folder) and
+        stat(file).st_size == message.size):
+        print("File already downloaded")
+        return None
+
+    #Download the file
+    print(file_name)
+    await client.download_media(message,
+        file=file, progress_callback=ProgressBar)
+
+    #Writing Database    
+    with db_conn:
+        add_file = (message_id, file)
+        dbm.insert_message(db_conn, add_file)
+
+    return file
 
 
-async def _download(client):
+async def _downloadAll(client, db_conn, start_date = None, media_type = None):
     '''Download every media in a chat'''
-    await client.connect()
-    async for message in client.iter_messages(chat,
-     offset_date=start_date, reverse=True):
+    #await client.connect()
+    async for message in client.iter_messages(chat, reverse=True,
+    offset_date=start_date):
 
         #Excluding non media messages
         if (not message.media or
-         not message.document or message.sticker):
+         message.sticker):
             continue
+
+        if(message.document.mime_type != media_type):
+            if media_type:
+                continue
         
+        message_id = message.id
+
         #Make variariabile easier to access
         message = message.document
         file_name = message.attributes[-1].file_name
@@ -29,12 +66,18 @@ async def _download(client):
         #Skip file already downloaded
         if(file_name in listdir(download_folder) and
          stat(file).st_size == message.size):
+            print("File already downloaded")
             continue
 
         #Download the file
         print(file_name)
         await client.download_media(message,
             file=file, progress_callback=ProgressBar)
+        
+        #Writing Database    
+        with db_conn:
+            add_file = (message_id, file)
+            dbm.insert_message(db_conn, add_file)
 
 
 def ProgressBar (iteration, total):
@@ -55,7 +98,8 @@ def SortingFolder(mime_type, download_path):
         if ("Video" not in listdir(download_path)):
             mkdir((new_path))
 
-    elif(mime_type == 'image/png'):
+    elif(mime_type == 'image/png' or
+    mime_type == 'image/jpeg'):
         new_path = path.join(download_path,"Pictures/")
         if ("Pictures" not in listdir(download_path)):
             mkdir((new_path))
